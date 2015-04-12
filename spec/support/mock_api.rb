c47dce8ec5b88f6ba4173b2837e7b30e7830330d
Laust Rud Jacobsen <laust@object.io>
2015-04-12 17:08:04 +0200

    For clarity, output what the current status of .mock!/.live! is during startup

--- spec/support/mock_api.rb
+++ spec/support/mock_api.rb
@@ -1,5 +1,7 @@
 if ENV['MOCK_API']
+  STDERR.puts 'Running with .mock! server'
   ApiWrapperExample::Client.mock!
 else
+  STDERR.puts 'Running with .live! server'
   ApiWrapperExample::Client.live!
 end
